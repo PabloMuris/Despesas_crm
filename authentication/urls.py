757ded9940119967ab1b69f78c396f42bff3f80f
pablo <pablo.murilo10@hotmail.com>
2024-07-16 13:38:34 -0300

    minor change

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,5 +6,5 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('register',RegistrationView.as_view(),name ='registrador'),
-    path('validate-username',csrf_exempt(UsernameValidationView.as_view(),name = 'validate-username'))
+    path('validate-username',csrf_exempt(UsernameValidationView.as_view()),name = 'validate-username')
 ]
--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'despesas','autentication'
+    'despesas','authentication',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 

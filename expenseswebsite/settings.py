8ddc02ab5cd66bb06d116c6b6408f1f2def36157
pablo <pablo.murilo10@hotmail.com>
2024-07-16 13:20:32 -0300

    erro

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -75,13 +75,9 @@ WSGI_APPLICATION = 'expenseswebsite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'expenses',
-        'USER':'root',
-        'PASSWORD':'admin',
-        'HOST':'localhost',
-        'PORT':'3306',
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 

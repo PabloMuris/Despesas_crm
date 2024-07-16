adb46f5a514efa750d55bca7a271559b693d8afc
Pablo <pablo.murilo10@hotmail.com>
2024-07-16 11:53:09 -0300

    register/js

--- expenseswebsite/static/js/authentication/register.js
+++ expenseswebsite/static/js/authentication/register.js
@@ -5,9 +5,18 @@ usernameFIeld.addEventListener('keyup',()=> {console.log('777777',777777);
 
 
 const usernameVal = e.target.value;
+if(usernameVal.length >0){
+fetch("/authentication/validate-username",{
 
+body: JSON.stringify({username : usernameVal}),
+method: "POST",
 
-
+})
+.then((res)=>res.json())
+then((data) => {
+    console.log("data",data)
+});
+}
 
 });
 
